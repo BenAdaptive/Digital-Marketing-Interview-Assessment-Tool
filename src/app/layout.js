d17acadd888ddd8eeba9bbdd91b1f1cdd7e17e14
BenAdaptive <ben.wattenbach@adaptive-digital.com>
2024-11-05 11:10:51 +0000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,1 +1,16 @@
-
+import { Inter } from 'next/font/google'
+ 
+const inter = Inter({ subsets: ['latin'] })
+ 
+export const metadata = {
+  title: 'Interview Assessment Tool',
+  description: 'Digital Marketing Interview Assessment Tool',
+}
+ 
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
